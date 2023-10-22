19b21e5119104dc422aa4ee1e1a2f3ea282ac977
dodamartin <146016722+dodamartin@users.noreply.github.com>
2023-10-22 09:40:51 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Weather from './components/Weather';
+import 'font-awesome/css/font-awesome.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
